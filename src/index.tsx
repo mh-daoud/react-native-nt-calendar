87fd99ed517f5115512a03a8c42a69b1c7349b03
= <=>
2024-03-03 12:04:29 +0300

    chore(types): fix type export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
 import NTCalendar from './NTCalendar';
+import * as NTCalendarTypes from './NTCalendar/types';
+import * as NTWeekHeaderComponentTypes from './NTCalendar/components/NTWeekHeader/types';
+import * as NTHeaderComponentTypes from './NTCalendar/components/NTHeaderComponent/types';
+import * as NTMonthComponentTypes from './NTCalendar/components/NTMonthComponent/types';
+import * as NTDayComponentTypes from './NTCalendar/components/NTDayComponent/types';
+
+export {
+  NTCalendarTypes,
+  NTWeekHeaderComponentTypes,
+  NTHeaderComponentTypes,
+  NTMonthComponentTypes,
+  NTDayComponentTypes,
+};
 
 export default NTCalendar;
